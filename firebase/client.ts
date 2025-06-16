6f352dd991faead5de337e82f6fbb4c6c85ec671
samararora15 <officialsamar404@gmail.com>
2025-06-16 12:39:53 +0530

    env files fix

--- firebase/client.ts
+++ firebase/client.ts
@@ -3,13 +3,13 @@ import { getAuth } from "firebase/auth";
 import {getFirestore} from 'firebase/firestore'
 
 const firebaseConfig = {
-  apiKey: "AIzaSyBHdMCXEHBLXLd_9W6bjih_aM48xuraRrg",
-  authDomain: "my-prepwise.firebaseapp.com",
-  projectId: "my-prepwise",
-  storageBucket: "my-prepwise.firebasestorage.app",
-  messagingSenderId: "603732008145",
-  appId: "1:603732008145:web:b48857df8b53cbd48e79f0",
-  measurementId: "G-8JR1C3SX6P"
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
+  measurementId: process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID,
 };
 
 // Initialize Firebase
